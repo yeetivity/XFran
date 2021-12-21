c4fb7c376b47d561f153f636fb2018f30974428e
JitsevE_KTH <jjve@kth.se>
2021-12-21 17:15:41 +0100

    Registering and logging out working in HomeScreenActivity.

--- app/src/main/java/kth/jjve/xfran/HomeScreenActivity.java
+++ app/src/main/java/kth/jjve/xfran/HomeScreenActivity.java
@@ -15,14 +15,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeScreenActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     /*_________ VIEW _________*/
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
     private Toolbar toolbar;
+    private Menu profileMenu;
+
+    /*_________ FBASE _________*/
+    FirebaseAuth firebaseAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +41,23 @@ public class HomeScreenActivity extends AppCompatActivity implements NavigationV
         navigationView = findViewById(R.id.nav_view);
         toolbar = findViewById(R.id.main_toolbar);
 
+
         /*------ INIT ------*/
         setSupportActionBar(toolbar);   // Initialise toolbar
         initNavMenu();                  // Initialise nav menu
+
+        /*------------ FBASE ------------*/
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if(firebaseAuth.getCurrentUser() != null){
+            profileMenu.findItem(R.id.nav_login).setVisible(false);
+            profileMenu.findItem(R.id.nav_profile).setVisible(true);
+            profileMenu.findItem(R.id.nav_logout).setVisible(true);
+        }
+
+        /*------------ LISTENERS ------------*/
+
+
     }
 
 
@@ -59,8 +80,15 @@ public class HomeScreenActivity extends AppCompatActivity implements NavigationV
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
         int id = menuItem.getItemId();
         if (id == R.id.nav_diary){
-            Intent intent = new Intent(HomeScreenActivity.this, TrainingDiaryActivity.class);
-            startActivity(intent);
+            startActivity(new Intent(HomeScreenActivity.this, TrainingDiaryActivity.class));
+        } else if (id == R.id.nav_login){
+            startActivity(new Intent(HomeScreenActivity.this, RegisterActivity.class));
+        } else if (id == R.id.nav_logout){
+            firebaseAuth.signOut();
+            Toast.makeText(HomeScreenActivity.this, "Logged out", Toast.LENGTH_SHORT).show();
+            profileMenu.findItem(R.id.nav_logout).setVisible(false);
+            profileMenu.findItem(R.id.nav_login).setVisible(true);
+            profileMenu.findItem(R.id.nav_profile).setVisible(false);
         }
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
@@ -70,9 +98,9 @@ public class HomeScreenActivity extends AppCompatActivity implements NavigationV
     private void initNavMenu(){
 
         //Hide or show items
-        Menu menu = navigationView.getMenu();
-        menu.findItem(R.id.nav_logout).setVisible(false);
-        menu.findItem(R.id.nav_profile).setVisible(false);
+        profileMenu = navigationView.getMenu();
+        profileMenu.findItem(R.id.nav_logout).setVisible(false);
+        profileMenu.findItem(R.id.nav_profile).setVisible(false);
 
         navigationView.bringToFront();
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
---
+++ app/src/main/java/kth/jjve/xfran/RegisterActivity.java
@@ -0,0 +1,86 @@
+package kth.jjve.xfran;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
+
+public class RegisterActivity extends AppCompatActivity {
+    EditText mFullName, mEmail, mPassword;
+    Button mRegisterBtn;
+    TextView mLoginBtn;
+    FirebaseAuth firebaseAuth;
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_register);
+
+        /*------------ HOOKS ------------*/
+        mFullName   = findViewById(R.id.reg_fullName);
+        mEmail      = findViewById(R.id.reg_email);
+        mPassword   = findViewById(R.id.reg_password);
+        mRegisterBtn= findViewById(R.id.reg_btnRegister);
+        mLoginBtn   = findViewById(R.id.reg_login);
+        progressBar = findViewById(R.id.progressBar);
+
+        /*------------ FBASE ------------*/
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if(firebaseAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(), HomeScreenActivity.class));
+            finish();
+        }
+
+        mRegisterBtn.setOnClickListener(v -> {
+            //Todo: check where this needs to be in MVVM structure
+            String email = mEmail.getText().toString().trim();
+            String password = mPassword.getText().toString().trim();
+
+            if(TextUtils.isEmpty(email)){
+                mEmail.setError("Email is Required");
+                return;
+            }
+            if(TextUtils.isEmpty(password)){
+                mPassword.setError("Password is Required");
+                return;
+            }
+
+            if(password.length() < 6){
+                mPassword.setError("Password must be at least 6 characters");
+                return;
+            }
+
+            progressBar.setVisibility(View.VISIBLE);
+
+            // register the user in firebase
+
+            firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+                if(task.isSuccessful()){
+                    Toast.makeText(RegisterActivity.this, "Account created", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(), HomeScreenActivity.class));
+                }else{
+                    Toast.makeText(RegisterActivity.this, "Error !" + Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        });
+
+    }
+}
