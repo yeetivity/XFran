360b7c7680e5a306e2f8ad0bfe75f0e63a1a74b7
JitsevE_KTH <jjve@kth.se>
2021-12-26 22:42:55 +0100

    Saving to firestore is now added to profile as well
    In profile activity user can store weight, length, first and last name

--- app/src/main/java/kth/jjve/xfran/repositories/UserRepo.java
+++ app/src/main/java/kth/jjve/xfran/repositories/UserRepo.java
@@ -1,14 +1,26 @@
 package kth.jjve.xfran.repositories;
 
+import android.util.Log;
+
 import androidx.lifecycle.MutableLiveData;
 
-import java.util.ArrayList;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import kth.jjve.xfran.models.UserProfile;
 
 public class UserRepo {
     private static UserRepo instance;
     private UserProfile userProfile = new UserProfile();
+    private MutableLiveData<UserProfile> up;
+
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore firebaseFirestore;
+    String userID;
 
     public static UserRepo getInstance(){
         if(instance == null){
@@ -19,7 +31,7 @@ public class UserRepo {
 
     public MutableLiveData<UserProfile> getUserProfile(){
         findUserProfile();
-        MutableLiveData<UserProfile> up = new MutableLiveData<>();
+        up = new MutableLiveData<>();
         up.setValue(userProfile);
 
         return up;
@@ -31,5 +43,24 @@ public class UserRepo {
 
     public void setUserProfile(String fn, String ln, String e, double w, int l){
         userProfile = new UserProfile(fn, ln, e, w, l);
+        if (up != null) up.setValue(userProfile);   //Add the user profile to the mutable data list
+
+        //Save the data to the database
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+        if (firebaseAuth.getCurrentUser() != null) {
+            userID = firebaseAuth.getCurrentUser().getUid();
+            DocumentReference documentReference = firebaseFirestore.collection("users").document(userID);
+            Map<String, Object> user = new HashMap<>();
+            user.put("fullName", userProfile.getFullName());
+            user.put("firstName", userProfile.getFirstName());
+            user.put("lastName", userProfile.getLastName());
+            user.put("weight", userProfile.getWeight());
+            user.put("length", userProfile.getLength());
+            documentReference.update(user).addOnSuccessListener(aVoid -> Log.d("UserRepo", "onSuccess: user profile is updated for "+ userID));
+        }else{
+            Log.i("UserRepo", "User is not logged in");
+        }
     }
 }
