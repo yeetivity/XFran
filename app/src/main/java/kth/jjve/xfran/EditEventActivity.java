232d9100519849cf90f892f47cd35e8b466559fe
elisa <elisa.k.perini@gmail.com>
2021-12-29 09:40:31 +0100

    added try/catch clause for empty event

--- app/src/main/java/kth/jjve/xfran/EditEventActivity.java
+++ app/src/main/java/kth/jjve/xfran/EditEventActivity.java
@@ -48,13 +48,18 @@ public class EditEventActivity extends AppCompatActivity
 
     private void saveEvent(){
         //method to save the name, start and end time of an event into a list
-        String s_eventName = eventName.getText().toString(); //given name
-        String startTime = eventStartTimeEdit.getText().toString(); //start time
-        String endTime = eventEndTimeEdit.getText().toString(); //end time
-        //TODO add some safety feature if time format wrong, currently need ie 04:00
-        Event newEvent = new Event(s_eventName, CalendarUtils.selectedDate, LocalTime.parse(startTime), LocalTime.parse(endTime));
-        Event.eventsList.add(newEvent);
-        Toast.makeText(getApplicationContext(), "event saved", Toast.LENGTH_SHORT).show();
+        try {
+            String s_eventName = eventName.getText().toString(); //given name
+            String startTime = eventStartTimeEdit.getText().toString(); //start time
+            String endTime = eventEndTimeEdit.getText().toString(); //end time
+            //TODO add some safety feature if time format wrong, currently need ie 04:00
+            Event newEvent = new Event(s_eventName, CalendarUtils.selectedDate, LocalTime.parse(startTime), LocalTime.parse(endTime));
+            Event.eventsList.add(newEvent);
+            Toast.makeText(getApplicationContext(), "event saved", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(), "no event info added", Toast.LENGTH_SHORT).show();
+        }
     }
 
     //TODO delete event
