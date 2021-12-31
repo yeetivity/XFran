c1132aa2088d5f23027758f0fc8f75af5748f76c
elisa <elisa.k.perini@gmail.com>
2021-12-31 18:16:07 +0100

    added some on the UI, calendar provider api not functional

--- app/src/main/java/kth/jjve/xfran/EditEventActivity.java
+++ app/src/main/java/kth/jjve/xfran/EditEventActivity.java
@@ -5,23 +5,38 @@ Activity to input and save the event
 Jitse van Esch, Elisa Perini & Mariah Sabioni
  */
 
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.icu.util.Calendar;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.text.TextUtils;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import java.time.LocalTime;
 
-import kth.jjve.xfran.models.Event;
+import kth.jjve.xfran.models.EventInApp;
 import kth.jjve.xfran.weeklycalendar.CalendarUtils;
 
-public class EditEventActivity extends AppCompatActivity
-{
+public class EditEventActivity extends AppCompatActivity {
+    private static final String LOG_TAG = EditEventActivity.class.getSimpleName();
     private EditText eventName, eventStartTimeEdit, eventEndTimeEdit;
+    private String s_eventName, startTime, endTime;
+
+    /*----- CALENDAR API -----*/
+    final int callbackId = 42;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,21 +51,24 @@ public class EditEventActivity extends AppCompatActivity
         eventStartTimeEdit = findViewById(R.id.eventStartTimeEdit);
         eventEndTimeEdit = findViewById(R.id.eventEndTimeEdit);
         Button buttonSave = findViewById(R.id.eventSave);
+        Button buttonClose = findViewById(R.id.close);
+        Button buttonExport = findViewById(R.id.savetogoogle);
 
         String s_date = "Date: " + CalendarUtils.cleanDate(CalendarUtils.selectedDate);
         eventDate.setText(s_date);
 
         /*-------- LISTENERS ------------*/
-        buttonSave.setOnClickListener(v -> {
-            saveEvent();
-        });
+        buttonClose.setOnClickListener(v -> { finish(); });
+        buttonSave.setOnClickListener(v -> { saveEvent(); });
+        buttonExport.setOnClickListener(v -> { saveEventToCalendar(); });
+
     }
 
-    private void saveEvent(){
-        //method to save the name, start and end time of an event into a list
-        String s_eventName = eventName.getText().toString(); //given name
-        String startTime = eventStartTimeEdit.getText().toString(); //start time
-        String endTime = eventEndTimeEdit.getText().toString(); //end time
+    private void setEventInApp(){
+        // method to obtain the events name and start/end time
+        s_eventName = eventName.getText().toString(); //given name
+        startTime = eventStartTimeEdit.getText().toString(); //start time
+        endTime = eventEndTimeEdit.getText().toString(); //end time
 
         if (TextUtils.isEmpty(s_eventName)){
             eventName.setError("Event name is required");
@@ -67,10 +85,28 @@ public class EditEventActivity extends AppCompatActivity
             eventEndTimeEdit.setError("End time is required");
             return;
         }
+    }
 
+    private void saveEvent(){
+        //method to save the name, start and end time of an event into a list, called by save button
+        setEventInApp();
+        createEventInApp();
+    }
+    //TODO delete event
+    //TODO save events into repository
+
+    private void saveEventToCalendar(){
+        // method to save the event in the calendar in the app and in the calendar provider, called by export buttom
+        setEventInApp();
+        calendarProviderAPI();
+        createEventInApp();
+    }
+
+    private void createEventInApp(){
+        // method to create the event and output it in the app
         try {
-            Event newEvent = new Event(s_eventName, CalendarUtils.selectedDate, LocalTime.parse(startTime), LocalTime.parse(endTime));
-            Event.eventsList.add(newEvent);
+            EventInApp newEventInApp = new EventInApp(s_eventName, CalendarUtils.selectedDate, LocalTime.parse(startTime), LocalTime.parse(endTime));
+            EventInApp.eventsList.add(newEventInApp);
             Toast.makeText(getApplicationContext(), "event saved", Toast.LENGTH_SHORT).show();
             finish();
         } catch (Exception e) {
@@ -79,7 +115,45 @@ public class EditEventActivity extends AppCompatActivity
         }
     }
 
-    //TODO delete event
-    //TODO save events into repository
+    private void calendarProviderAPI(){
+        long calID = 3;
+        long startMillis = 0;
+        long endMillis = 0;
+        Calendar beginTime = Calendar.getInstance();
+        //TODO remove hardcoding of date
+        beginTime.set(2022, 1, 1, 7, 30);
+        startMillis = beginTime.getTimeInMillis();
+        Calendar endTime = Calendar.getInstance();
+        endTime.set(2022, 1, 1, 8, 45);
+        endMillis = endTime.getTimeInMillis();
+
+        checkPermission(callbackId, Manifest.permission.READ_CALENDAR, Manifest.permission.WRITE_CALENDAR);
+
+        ContentResolver cr = getContentResolver();
+        ContentValues values = new ContentValues();
+        values.put(CalendarContract.Events.DTSTART, startMillis);
+        values.put(CalendarContract.Events.DTEND, endMillis);
+        values.put(CalendarContract.Events.TITLE, "Jazzercise");
+        values.put(CalendarContract.Events.DESCRIPTION, "Group workout");
+        values.put(CalendarContract.Events.CALENDAR_ID, calID);
+        values.put(CalendarContract.Events.EVENT_TIMEZONE, "America/Los_Angeles");
+        Uri uri = cr.insert(CalendarContract.Events.CONTENT_URI, values);
+
+        long eventID = Long.parseLong(uri.getLastPathSegment());
+
+        //Toast.makeText(getApplicationContext(), (int) eventID, Toast.LENGTH_SHORT).show();
+        Log.i(LOG_TAG, String.valueOf(eventID));
+
+    }
+
+    private void checkPermission(int callbackId, String... permissionsId) {
+        // method to check if permissions granted, needed for "dangerous" permission like write calendar
+        boolean permissions = true;
+        for (String p : permissionsId) {
+            permissions = permissions && ContextCompat.checkSelfPermission(this, p) == PERMISSION_GRANTED;
+        }
+        if (!permissions)
+            ActivityCompat.requestPermissions(this, permissionsId, callbackId);
+    }
 
 }
