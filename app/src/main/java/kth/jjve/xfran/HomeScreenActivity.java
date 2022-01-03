52858955dfc770fd8dadf7b9dc3a52f718a887f4
JitsevE_KTH <jjve@kth.se>
2022-01-03 22:20:49 +0100

    Added workout of the day to the homescreen

--- app/src/main/java/kth/jjve/xfran/HomeScreenActivity.java
+++ app/src/main/java/kth/jjve/xfran/HomeScreenActivity.java
@@ -10,8 +10,20 @@ Mariah Sabioni
 
 import android.os.Bundle;
 import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import kth.jjve.xfran.viewmodels.HomeVM;
 
 public class HomeScreenActivity extends BaseActivity {
+    TextView WODTitle, WODDescription, WODExercises, Username;
+    RecyclerView PlannedWorkouts;
+    HomeVM homeVM;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +31,27 @@ public class HomeScreenActivity extends BaseActivity {
 
         FrameLayout contentFrameLayout = findViewById(R.id.content_frame);
         getLayoutInflater().inflate(R.layout.act_home, contentFrameLayout);
+
+        /*----- HOOKS -----*/
+        WODTitle = findViewById(R.id.home_WODTitle);
+        WODDescription = findViewById(R.id.home_WODDescription);
+        WODExercises = findViewById(R.id.home_WODExercises);
+        Username = findViewById(R.id.home_username);
+        PlannedWorkouts = findViewById(R.id.home_rv);
+
+        /*-----  VM  -----*/
+        homeVM = ViewModelProviders.of(this).get(HomeVM.class);
+        homeVM.init();
+        homeVM.getUserName().observe(this, s -> Username.setText(s));
+        homeVM.getWODTitle().observe(this, s -> WODTitle.setText(s));
+        homeVM.getWODDescription().observe(this, s -> WODDescription.setText(s));
+        homeVM.getWODExercises().observe(this, ls -> {
+            StringBuilder exercises = new StringBuilder();
+            for (String s : ls){
+                exercises.append(s).append("\n");
+            }
+            WODExercises.setText(exercises);
+        });
 
         navigationView.setCheckedItem(R.id.nav_home);
     }
--- app/src/main/java/kth/jjve/xfran/viewmodels/HomeVM.java
+++ app/src/main/java/kth/jjve/xfran/viewmodels/HomeVM.java
@@ -1,5 +1,90 @@
 package kth.jjve.xfran.viewmodels;
 
-public class HomeVM {
+import android.app.Application;
 
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Random;
+
+import kth.jjve.xfran.models.Workout;
+import kth.jjve.xfran.repositories.UserRepo;
+import kth.jjve.xfran.repositories.WorkoutRepo;
+
+public class HomeVM extends AndroidViewModel {
+    private MutableLiveData<String> mUserName, mWODTitle, mWODDescription;
+    private MutableLiveData<ArrayList<String>> mWODExercises;
+    private LocalDate mDate;
+    private int wodChooser, previousWOD;
+
+    private UserRepo mUserRepo;
+    private WorkoutRepo mWorkoutRepo;
+
+
+    public HomeVM(@NonNull Application application) {
+        super(application);
+    }
+
+    public void init() {
+        mUserRepo = UserRepo.getInstance();
+        mUserName = mUserRepo.getUserName();
+
+        mWorkoutRepo = WorkoutRepo.getInstance();
+        try {
+            setWOD(mWorkoutRepo.getWorkouts());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setWOD(MutableLiveData<List<Workout>> mWorkouts) {
+        // Todo: mDate now gets overwritten when activity is destroyed, find a way to make sure it does not
+        if (LocalDate.now() != mDate) {
+            mDate = LocalDate.now();
+
+            // Get a random workout
+            Random rand = new Random();
+            while (wodChooser == previousWOD) {
+                wodChooser = rand.nextInt(Objects.requireNonNull(mWorkouts.getValue()).size());
+            }
+
+            Workout WOD = Objects.requireNonNull(mWorkouts.getValue()).get(wodChooser);
+            setWODDetails(WOD);
+            previousWOD = wodChooser;
+        }
+
+    }
+
+    private void setWODDetails(Workout wod) {
+        mWODTitle = new MutableLiveData<>();
+        mWODDescription = new MutableLiveData<>();
+        mWODExercises = new MutableLiveData<>();
+
+        mWODTitle.setValue(wod.getTitle());
+        mWODDescription.setValue(wod.getType());
+        mWODExercises.setValue(wod.getExercises());
+    }
+
+    public LiveData<String> getUserName() {
+        return mUserName;
+    }
+
+    public LiveData<String> getWODTitle() {
+        return mWODTitle;
+    }
+
+    public LiveData<String> getWODDescription() {
+        return mWODDescription;
+    }
+
+    public LiveData<ArrayList<String>> getWODExercises() {
+        return mWODExercises;
+    }
 }
